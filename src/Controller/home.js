c3ca90c4cd74d4f9220db0e095844ee208300b79
ginaesps <respinosa7@ucol.mx>
2021-06-23 09:29:43 -0500

    check adjustments and personal comments

--- src/Controller/home.js
+++ src/Controller/home.js
@@ -5,78 +5,77 @@ const socket = io("http://127.0.0.1:5001",{ //client socket
     withCredentials: true,
     extraHeaders: {
         "my-custom-header": "abcd"
-    }
+    }//configuraciones de obj socket creado a partir de instancia
 });
 
-exports.home = async function(req, res, next){
-    mysql.query('select * from lista where 1', (err, rows, fields) =>{
-        let listas = [];
-        if(!err){
-            res.render('Home',{
-                lista:rows
-            }
-            );
-        }
-        else{
-            res.render('home',{
-                lista:listas
-            }
-            );
-        }
-    });
-};
-
-exports.add = async function(req, res, next){
-    let {Name, Description, Autor, Estado, Date} = req.body;
-    mysql.query('INSERT INTO lista(Name, Description, Autor, Estado, Date) VALUES (?,?,?,?,?)',[Name, Description, Autor, Estado, Date], (err, rows, fields) =>{
-        if(!err){
-            mysql.query('select * from lista where Description = ? && Date = ?',[Description, Date], (err, rows, fields) =>{
-                if(!err){
-                    socket.emit('AddtoDo', rows);
+module.exports = {
+    home: (req, res, next) => {
+        mysql.query('select * from lista where 1', (err, rows, fields) =>{
+            let listas = [];
+            if(!err){
+                res.render('Home',{
+                    lista:rows
                 }
-            });
-            res.status(200).json({mensaje:"Guardado"});
+                );
+            }
+            else{
+                res.render('Home',{
+                    lista:listas
+                }
+                );
+            }
+        });
+    },
+    add: (req, res, next) => {
+        let {Name, Description, Autor, Estado, Date} = req.body;
+        mysql.query('INSERT INTO lista(Name, Description, Autor, Estado, Date) VALUES (?,?,?,?,?)',[Name, Description, Autor, Estado, Date], (err, rows, fields) =>{
+            if(!err){
+                mysql.query('select * from lista where Description = ? && Date = ?',[Description, Date], (err, rows, fields) =>{
+                    if(!err){
+                        socket.emit('AddtoDo', rows);
+                    }
+                });
+                res.status(200).json({mensaje:"Guardado"});
+            }
+            else{
+                res.status(400).json({mensaje:"Error al Guardar"});
+            }
+        });
+    },
+    completar: (req, res, next) => {
+        let id = req.params.id;
+        let {check} = req.body;
+        if(check == 0){
+            check = 0;
         }
         else{
-            res.status(400).json({mensaje:"Error al Guardar"});
+            check = 1;
         }
-    });
-};
-
-exports.completar = async function(req, res, next){
-    let id = req.params.id;
-    let {check} = req.body;
-    if(check == 0){
-        check = 0;
+        let datos = {
+            id,
+            check
+        }
+        mysql.query('UPDATE lista SET `Estado`=? WHERE id = ?',[check, id], (err, rows, fields) =>{
+            if(!err){
+                socket.emit('UpdateEstado',datos);
+                res.status(200).json({mensaje:"Guardado"});
+            }
+            else{
+                res.status(400).json({mensaje:"Error al Guardar"});
+            }
+        });
+    },
+    delete: (req, res, next) => {
+        let id = req.params.id;
+        console.log(id);
+        mysql.query('DELETE FROM lista WHERE id = ?',[id], (err, rows, fields) =>{
+            if(!err){
+                socket.emit('eliminar',id);
+                res.status(200).json({mensaje:"Eliminado"});
+            }
+            else{
+                res.status(400).json({mensaje:"Error al Eliminar"});
+            }
+        });
     }
-    else{
-        check = 1;
-    }
-    let datos = {
-        id,
-        check
-    }
-    mysql.query('UPDATE lista SET `Estado`=? WHERE id = ?',[check, id], (err, rows, fields) =>{
-        if(!err){
-            socket.emit('UpdateEstado',datos);
-            res.status(200).json({mensaje:"Guardado"});
-        }
-        else{
-            res.status(400).json({mensaje:"Error al Guardar"});
-        }
-    });
-};
-
-exports.delete = async function(req, res, next){
-    let id = req.params.id;
-    console.log(id);
-    mysql.query('DELETE FROM lista WHERE id = ?',[id], (err, rows, fields) =>{
-        if(!err){
-            socket.emit('eliminar',id);
-            res.status(200).json({mensaje:"Eliminado"});
-        }
-        else{
-            res.status(400).json({mensaje:"Error al Eliminar"});
-        }
-    });
-}; 
+}
--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const express = require('express'); //require express
+const app = express(); // instanciar express a través de una variable llamada app   
+const http = require('http'); //allows to create server
+const server = http.createServer(app); // socket.io needs httpServer
 const path = require('path'); //will allow to make routes
-const engine = require('ejs-mate'); //search engine
+const engine = require('ejs-mate'); //search engine require
 const io = require('socket.io')(server,{ // web socket
     cors: { //allow http requests from an app to another
         origin: "*",
@@ -15,9 +15,10 @@ const io = require('socket.io')(server,{ // web socket
 
 require('./DataBase/Conexion');
 
-app.set('views',path.join(__dirname,'Views')); // joins routes with views 
-app.engine('ejs',engine);
-app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'Views')); //helps to not considere the / or \ that are used in directions depending on the OS and tells to visit a directory, then 
+// __dirname = the actual direction and then the name of where to go
+app.engine('ejs',engine); // sets the view engine that will be used
+app.set('view engine','ejs'); //view engine establishment
 app.use('/Public',express.static(path.join(__dirname,'Public')));
 app.use(express.json());
 app.use(express.urlencoded({extended : false }));
@@ -40,7 +41,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const port = process.env.PORT || 5001;
-server.listen(port, '0.0.0.0', function(){
-    console.log("Server ON: " + port);
+const port = process.env.PORT || 5001; // initialize port
+server.listen(port, '0.0.0.0', function(){ // IP address allows to receive requests from any IP in case that u connect it to a server
+    console.log(`Server ON: ${port}`);
 });
